5200152c6fc48920acb8a0c77ebb02b9ecf53752
Gautam  Sapre <gautamsapre@Gautams-MBP.attlocal.net>
2019-08-21 10:32:11 -0500

    try 9

--- form.php
+++ form.php
@@ -1,27 +1,13 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
-
-
-$email_from = 'gautamsapre@outlook.com';
-
-$email_subject = "New Form Submission";
-
-$email_body = "User Name: $name.\n".
-"User Email: $visitor_email.\n".
-"User Message: $message.\n";
-
-
-$to = "gautamsapre@gmail.com";
-
-$headers = "From: $email_from \r\n";
-
-$headers .= "Reply To: $visitor_email \r\n";
-
-mail($to,email_subject,$email_body,$headers);
-
-header("Location: index.html");
-
-
-?>
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $message = $_POST['message'];
+    
+    $mailTo = "gautamsapre@outlook.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+    
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html?mailsend");
+    }

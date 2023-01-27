0fa16c15f6f8d014ea3883c455a9bc7c4c3b6d3e
Gautam  Sapre <gautamsapre@Gautams-MBP.attlocal.net>
2023-01-27 11:34:31 -0600

    About me section added

--- react-portfolio/src/components/about/About.jsx
+++ react-portfolio/src/components/about/About.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './about.css'
+import self from '../../assets/self2.jpg'
+import {BsLinkedin, BsFacebook, BsInstagram} from 'react-icons/bs'
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <img src={self} alt="About Me image" />
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__cards">
+            </article>
+          </div>
+          <p>
+
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div className="social__media">
+          <a href="https://www.linkedin.com/in/gautam-sapre" target="_blank"> <BsLinkedin /> </a>
+          <a href="https://www.facebook.com/gautam.sapre.7" target="_blank"> <BsFacebook /> </a>
+          <a href="https://www.instagram.com/gautam_sapre/" target="_blank"> <BsInstagram /> </a>
+        </div>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
--- react-portfolio/src/components/nav/Nav.jsx
+++ react-portfolio/src/components/nav/Nav.jsx
@@ -5,14 +5,17 @@ import {BsFillPersonFill} from 'react-icons/bs';
 import {AiFillProject} from 'react-icons/ai';
 import {MdWork} from 'react-icons/md';
 import {BsFillChatDotsFill} from 'react-icons/bs';
+import {useState} from 'react'
+
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#header"><FiHome/></a>
-      <a href="#about"><BsFillPersonFill/></a>
-      <a href="#experience"><MdWork/></a>
-      <a href="#projects"><AiFillProject/></a>
-      <a href="#contact"><BsFillChatDotsFill/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><FiHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BsFillPersonFill/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdWork/></a>
+      <a href="#projects" onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ''}><AiFillProject/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BsFillChatDotsFill/></a>
     </nav>
   )
 }

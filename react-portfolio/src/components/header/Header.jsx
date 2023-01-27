d7cc1b0798bcaa348f966abb43ac942621cd0794
Gautam  Sapre <gautamsapre@Gautams-MBP.attlocal.net>
2023-01-26 22:56:02 -0600

    Added Header component

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import pic from '../../assets/Self.png'
+import HeaderSocials from './HeaderSocials'
+
 const Header = () => {
   return (
-    <div>Header </div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm </h5>
+        <h1>Gautam</h1>
+        <h5 className="text-light">Student | Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="myPic">
+          <img src={pic} alt="My Pic" />
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+        
+      </div>
+    </header>
   )
 }
 

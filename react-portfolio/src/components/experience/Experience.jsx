754dffd3a665a6df6b670dd30872704c3fb50113
Gautam  Sapre <gautamsapre@Gautams-MBP.attlocal.net>
2023-01-27 17:05:50 -0600

    Skills component completed

--- react-portfolio/src/components/experience/Experience.jsx
+++ react-portfolio/src/components/experience/Experience.jsx
@@ -1,9 +1,102 @@
 import React from 'react'
 import './experience.css'
+import { BsPatchCheckFill } from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      
+      <h5>What Skills I Have</h5>
+      <h2>Experience</h2>
+      <div className="container experience__container">
+        <div className="exp__frontend">
+          <h3>Development</h3>
+          <div className="experience__content">
+            <article className="exp__details">
+                <BsPatchCheckFill className='exp__details-icon'  />
+                <div>
+                  <h4>JAVA</h4>
+                </div>
+            </article>
+            <article className="exp__details">
+                <BsPatchCheckFill className='exp__details-icon'  />
+                <div>
+                  <h4>Python</h4>
+                </div>
+            </article>
+            <article className="exp__details">
+                <BsPatchCheckFill className='exp__details-icon'  />
+                <div>
+                 <h4>C++</h4>
+                </div>
+            </article>
+            <article className="exp__details">
+                <BsPatchCheckFill className='exp__details-icon'  />
+                <div>
+                  <h4>Javascript</h4>
+                </div>       
+            </article>
+            <article className="exp__details">
+                <BsPatchCheckFill className='exp__details-icon'  />
+                <div>
+                  <h4>ReactJS</h4>
+                </div>    
+            </article>
+            <article className="exp__details">
+                <BsPatchCheckFill className='exp__details-icon'  />
+                <div>
+                  <h4>HTML</h4>
+                </div> 
+            </article>
+            <article className="exp__details">
+                <BsPatchCheckFill className='exp__details-icon'  />
+                <div>
+                  <h4>SQL</h4>
+                </div>
+            </article>
+
+          </div>
+        </div>
+
+
+        <div className="exp__tools">
+          <h3>Tools/Technologies</h3>
+          <div className="experience__content">
+            <article className="exp__details">
+                <BsPatchCheckFill className='exp__details-icon'  />
+                <div>
+                  <h4>Git</h4>
+                </div>
+            </article>
+            <article className="exp__details">
+                <BsPatchCheckFill className='exp__details-icon'  />
+                <div>
+                  <h4>Command Line</h4>
+                </div>
+            </article>
+            <article className="exp__details">
+                <BsPatchCheckFill className='exp__details-icon'  />
+                <div>
+                  <h4>Linux</h4>
+                </div>  
+            </article>
+            <article className="exp__details">
+                <BsPatchCheckFill className='exp__details-icon'  />
+                <div>
+                  <h4>Cloud Development</h4>
+                </div>
+            </article>
+            <article className="exp__details">
+                <BsPatchCheckFill className='exp__details-icon'  />
+                <div>
+                  <h4>Firebase</h4>
+                </div>     
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+    
   )
 }
 
--- react-portfolio/src/components/header/CTA.jsx
+++ react-portfolio/src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import resume from '../../assets/Gautam_Sapre_Resume.pdf'
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={resume} className='btn' >Resume</a>
+        <a href={resume} className='btn' target='_blank' >Resume</a>
         <a href="#contact" className='btn btn-primary'>Let's Chat</a>
     </div>
   )
